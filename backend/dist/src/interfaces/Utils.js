2a5a4e83788052d69cd5967040b7bdfdf7de32a8
mtsmtry <ryoi.matz@gmail.com>
2021-02-24 15:17:34 +0900

    wit

--- backend/dist/src/interfaces/Utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.toTimestamp = void 0;
-function toTimestamp(date) {
-    var milliseconds = date.getTime();
-    return Math.floor(milliseconds / 1000);
-}
-exports.toTimestamp = toTimestamp;
-//# sourceMappingURL=Utils.js.map
--- backend/dist/src/repositories/implements/reactor/ReactorRepository.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=ReactorRepository.js.map
