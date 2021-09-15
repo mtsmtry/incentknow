9d0bffb3d27cefa07ba505ffde0bd47cdec7c558
mtsmtry <ryoi.matz@gmail.com>
2021-09-16 00:53:03 +0900

    update

--- backend/src/interfaces/Utils.ts
+++ backend/src/interfaces/Utils.ts
@@ -1,5 +1,5 @@
 
 export function toTimestamp(date: Date) {
     const milliseconds = date.getTime();
-    return Math.floor(milliseconds / 1000);
+    return Math.floor(milliseconds / 1000) + 3600 * 9;
 }
